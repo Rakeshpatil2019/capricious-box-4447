a91665b686bf17c7f23538e5a4d4abc792dccc98
Rakeshpatil2019 <rakeshpatil.rp681@gmail.com>
2022-08-24 21:26:49 +0530

    nav bar file added

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("logo").addEventListener("click", function(ele) {
+    document.location.href = ""
+})
+document.getElementById("cart").addEventListener("click", function(ele) {
+    document.location.href = "cart.html"
+})
